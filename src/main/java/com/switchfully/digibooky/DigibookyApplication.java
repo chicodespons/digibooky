49651b15a7f8c1b304bd21bb04ee37302cbdb798
chicodespons <pieterdedycker@gmail.com>
2022-11-22 10:53:48 +0100

    first commit

---
+++ src/main/java/com/switchfully/digibooky/DigibookyApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.digibooky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigibookyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigibookyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfully/digibooky/DigibookyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.digibooky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigibookyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
