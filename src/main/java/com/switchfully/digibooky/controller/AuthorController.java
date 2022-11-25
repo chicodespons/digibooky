2fb7897220b6a09818a301e1debe90621edf5431
Sven <114080055+codingsven@users.noreply.github.com>
2022-11-25 10:47:31 +0100

    deleted authorcontroler

--- src/main/java/com/switchfully/digibooky/controller/AuthorController.java
+++
@@ -1,9 +0,0 @@
-package com.switchfully.digibooky.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path = "/author")
-public class AuthorController {
-}
