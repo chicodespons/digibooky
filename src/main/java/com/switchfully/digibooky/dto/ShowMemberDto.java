55b19b6579141b8f21f899d7fcccc4b2ec21748d
Miri913 <haddarmiriam@gmail.com>
2022-11-25 10:40:09 +0100

    changes ShowMemberDto
    Signed-off-by: Miri913 <haddarmiriam@gmail.com>

--- src/main/java/com/switchfully/digibooky/dto/ShowMemberDto.java
+++ src/main/java/com/switchfully/digibooky/dto/ShowMemberDto.java
@@ -1,5 +1,30 @@
 package com.switchfully.digibooky.dto;
 
+import com.switchfully.digibooky.models.LentBook;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class ShowMemberDto {
+
+    private  String password;
+    private  String lastname;
+    private  String firstname;
+    private  String email;
+    private LentBook lentBook;
+
+    public ShowMemberDto(String password, String lastname, String firstname, String email, LentBook lentBook) {
+        this.password = password;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.email = email;
+        this.lentBook = lentBook;
+    }
+
 
 }
